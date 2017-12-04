25e053eafa5901b7ad486bae724796d2f02d988f
mrsaxmannjr <mrsaxmannjr@gmail.com>
2017-12-04 10:05:35 -0700

    fixed typo

--- test/tddwarmup.test.js
+++ test/tddwarmup.test.js
@@ -3,6 +3,9 @@ const code = require("../app");
 
 describe("#arrLengthDoubled", function() {
   it("the function returns the doubled length of an array.", function() {
-    assert.equal(code.arrLengthDoubled["these", "are", "words", "in", "the", "array"]), 12
-  })
-})
+    assert.equal(
+      code.arrLengthDoubled(["these", "are", "words", "in", "the", "array"]),
+      12
+    );
+  });
+});
